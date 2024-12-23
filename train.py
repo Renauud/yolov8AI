60275d711f76030737cabbe85afecad9f16e31c0
Renaud <rzell57@mail.com>
2024-12-23 20:07:19 +0100

    ai trained w same data set but 171epochs instead of 30

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="data.yaml", epochs=30, imgsz=640)
+results = model.train(data="data.yaml", epochs=300, imgsz=640)
